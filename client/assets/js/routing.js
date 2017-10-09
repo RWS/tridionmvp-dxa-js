abcdf968da54436366587419d20e93fc132fd6f1
Frank Taylor <frank@tahzoo.com>
2017-10-09 17:26:17 -0500

    appeases the linter with const, single quote strings, and converted indentation

--- client/assets/js/routing.js
+++ client/assets/js/routing.js
@@ -1,9 +1,9 @@
-DXAJS.RouteHandler  = function(event){
-    event.preventDefault();
-    var targetLink = event.target || event.srcElement;
-    var urlToRoute = targetLink.getAttribute("href");
-   
-    // Load data for URL and process MVC
+DXAJS.RouteHandler = function (event) {
+  event.preventDefault();
+  const targetLink = event.target || event.srcElement;
+  const urlToRoute = targetLink.getAttribute('href');
 
-    console.log("Routing to: " + urlToRoute);
-}
+  // Load data for URL and process MVC
+
+  console.log('Routing to: ' + urlToRoute);
+};
